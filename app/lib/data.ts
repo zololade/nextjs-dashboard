ee5e98307c2953e51b392f263c1cbac601e03885
Akingbogun Ololade <73138104+zololade@users.noreply.github.com>
2024-10-06 14:31:49 +0100

    feat: Implement dynamic search, pagination, and actions
    This implements dynamic search, server-side pagination for the invoices table, and adds actions for creating, updating, and deleting invoices.
    
    The dashboard page is removed in favor of a client-side routed experience within the invoices page, providing a more streamlined user flow. Additionally, experimental partial pre-rendering is enabled for improved performance and SEO.

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -19,7 +19,7 @@ export async function fetchRevenue() {
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch completed after 3 seconds.');
+    // console.log("Data fetch completed after 3 seconds.");
 
     return data.rows;
   } catch (error) {
